97aa7cf37f0338fe5a4470d0b7c22fb6b651cff9
Jimly Firdaus <101716223+Jimly-Firdaus@users.noreply.github.com>
2023-01-17 16:49:38 +0700

    refactor: delete compilation .js

--- src/lib/readline.ts
+++
@@ -1,9 +0,0 @@
-import * as readline from "readline"
-
-/**
- * Readline module built in Node
- */
-export const stdin =  readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
