e252ae4f6ec0f406078a3d512d20c84378e830a5
Jimly-Firdaus <13521102@std.stei.itb.ac.id>
2023-01-17 16:43:43 +0700

    feat: prompt input from console

--- src/lib/readline.ts
+++ src/lib/readline.ts
@@ -1,4 +1,5 @@
 import * as readline from "readline"
+import { toCapitallize } from "./string_modifier";
 
 /**
  * Readline module built in Node
@@ -7,3 +8,14 @@ export const stdin =  readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
+
+/**
+ * input from console
+ * @async true
+ * @param user_data captured string from console
+ * @returns full string of user input
+ */
+export const inputData = async (user_data: string) => {
+    let userInput: string = await new Promise(resolve => stdin.question(`Enter ${user_data.toLowerCase()}: `, resolve));
+    return toCapitallize(userInput);
+}
