f268822415036f8fea03f700cc789df3856728a5
Jimly-Firdaus <13521102@std.stei.itb.ac.id>
2023-01-27 23:53:24 +0700

    fix(user): delete decorators.ts (unused)

--- src/lib/constant/decorators.ts
+++
@@ -1,4 +0,0 @@
-// cc: https://stackoverflow.com/questions/63915237/annotate-method-override-in-typescript-to-make-easy-in-code-reviews
-export const Override = (target: Object, propertyKey: string | symbol, descriptor: PropertyDescriptor) => {
-    descriptor.writable = true;
-}
