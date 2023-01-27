5b248eed9d0ff1b71d3a45092bfd58ab94b5e736
Jimly-Firdaus <13521102@std.stei.itb.ac.id>
2023-01-27 19:32:45 +0700

    feat(user): add decorators

--- src/lib/constant/decorators.ts
+++ src/lib/constant/decorators.ts
@@ -1,9 +1,4 @@
 // cc: https://stackoverflow.com/questions/63915237/annotate-method-override-in-typescript-to-make-easy-in-code-reviews
-export const Override = () => {
-    return function(target: Object, key: string, descriptor: PropertyDescriptor) {
-        descriptor.value = function() {
-            const baseMethod = Object.getPrototypeOf(target)[key];
-            return baseMethod.apply(this, arguments);
-        }
-    }
+export const Override = (target: Object, propertyKey: string | symbol, descriptor: PropertyDescriptor) => {
+    descriptor.writable = true;
 }
